2fe7e332b3992d7a537be95b9a934d9f0369000a
Ari <agenthundmc@gmail.com>
2026-08-18 11:04:33 +0200

    Fixed some errors

--- install.bat
+++ install.bat
@@ -3,17 +3,17 @@ chcp 65001 >nul
 setlocal enabledelayedexpansion
 echo LOGO
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-echo @github.com/repo
-echo @holyari & @komethere
+echo @github.com/komethere/EntiretyView
+echo @holyarii & @komethere
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 echo Loading...
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-echo REQUIREMENTS.txt INSTALL HERE
+echo requirements.txt INSTALL HERE
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 echo Succesfully installed all dependencies!
 echo.
-set /p id=Would you like to start PROGRAM (Y/N): 
+set /p id=Would you like to start EntiretyView? (Y/N): 
 if /i "%id%"=="Y" (
     py ./script.py
 )
